9f2a75cf390649819b0b3f84526cc9bade218c98
Cyanide <alphaghost.ag@gmail.com>
2026-08-24 15:29:56 +0530

    metadata parsing

---
+++ backend/target/debug/build/quote-91fffa6232de600c/build_script_build-91fffa6232de600c.d
@@ -0,0 +1,5 @@
+C:\Users\movva\Documents\GitHub\music\backend\target\debug\build\quote-91fffa6232de600c\build_script_build-91fffa6232de600c.d: C:\Users\movva\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quote-1.0.47\build.rs
+
+C:\Users\movva\Documents\GitHub\music\backend\target\debug\build\quote-91fffa6232de600c\build_script_build-91fffa6232de600c.exe: C:\Users\movva\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quote-1.0.47\build.rs
+
+C:\Users\movva\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quote-1.0.47\build.rs:
---
+++ backend/target/debug/deps/cfg_if-b7349115d2a20ae0.d
@@ -0,0 +1,5 @@
+C:\Users\movva\Documents\GitHub\music\backend\target\debug\deps\cfg_if-b7349115d2a20ae0.d: C:\Users\movva\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+C:\Users\movva\Documents\GitHub\music\backend\target\debug\deps\libcfg_if-b7349115d2a20ae0.rmeta: C:\Users\movva\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+C:\Users\movva\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs:
